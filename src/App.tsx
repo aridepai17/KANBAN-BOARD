22ea241b68546e3a125b07d6aeac612405456cf5
aridepai17 <advaithdepai26@gmail.com>
2025-04-28 00:08:17 +0530

    Final touches

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,85 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-import  TaskCard from './components/TaskCard'
-import  { tasks as intialTasks, statuses , Task, Status } from './utils/data-tasks'
-
+import TaskCard from './components/TaskCard'
+import { Status, statuses, Task } from './utils/data-tasks'
 
 function App() {
-  const [tasks, setTasks] = useState<Task[]>(intialTasks)
+  const [tasks, setTasks] = useState<Task[]>([])
   const columns = statuses.map((status) => {
     const tasksInColumn = tasks.filter((task) => task.status === status)
     return {
-      title: status,
+      status,
       tasks: tasksInColumn
     }
   })
 
+  useEffect(() => {
+    fetch('http://localhost:3000/tasks').then((res) => res.json()).then((data) => {
+      setTasks(data)
+    })
+  }, [])
+
   const updateTask = (task: Task) => {
+    fetch(`http://localhost:3000/tasks/${task.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
     const updatedTasks = tasks.map((t) => {
-      return t.id === task.id ? { ...t, ...task } : t
+      return t.id === task.id ? task : t
     })
     setTasks(updatedTasks)
   }
 
   const handleDrop = (e: React.DragEvent<HTMLDivElement>, status: Status) => {
     e.preventDefault()
+    setCurrentlyHoveringOver(null)
     const id = e.dataTransfer.getData("id")
     const task = tasks.find((task) => task.id === id)
-    if(task){
+    if(task) {
       updateTask({...task, status})
     }
-    
+  }
+
+  const [currentlyHoveringOver, setCurrentlyHoveringOver] = useState<Status | null>(null)
+  const handleDragEnter = (status: Status) => {
+    setCurrentlyHoveringOver(status)
   }
 
   return (
-    <div className="flex divide-x">
-      {columns.map((column) => (
-        <div onDrop={(e) => handleDrop(e, column.title)} onDragOver={(e) => e.preventDefault()}>
-        <div>
-          <div className = "flex justify-between text-3l p-2 font-bold text-gray-500">
-          <h2 className='capitalize'>{column.title}</h2>
-          {column.tasks.reduce((total, task) => total + (task.points || 0), 0)}
-          </div>
-          {column.tasks.map((task) => (
-          <TaskCard 
-            task={task} 
-            updateTask={updateTask}/>
+    <div className="flex flex-col min-h-screen">
+      <h1 className="text-center text-4xl py-4">KANBAN BOARD</h1>
+      <div className="flex flex-1 justify-center">
+        <div className="flex divide-x w-full max-w-6xl mx-auto">
+          {columns.map((column) => (
+            <div
+              key={column.status}
+              className="flex-1"
+              onDrop={(e) => handleDrop(e, column.status)}
+              onDragOver={(e) => e.preventDefault()}
+              onDragEnter={() => handleDragEnter(column.status)}
+            >
+              <div className="flex justify-between text-2xl p-2 font-bold text-white-500">
+                <h2 className="capitalize text-center w-full">{column.status}</h2>
+                <div className="ml-2">{column.tasks.reduce((total, task) => total + (task?.points || 0), 0)}</div>
+              </div>
+              <div className={`h-full p-2 ${currentlyHoveringOver === column.status ? 'bg-gray-200' : ''}`}>
+              {column.tasks.map((task) => (
+                <TaskCard
+                  key={task.id}
+                  task={task}
+                  updateTask={updateTask}
+                />
+              ))}
+              </div>
+            </div>
           ))}
         </div>
-        </div>
-      ))}
+      </div>
     </div>
   )
 }
+
 export default App
