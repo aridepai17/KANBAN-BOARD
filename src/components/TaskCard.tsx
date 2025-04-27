7d7ed37ee7fc97ca2f6f18b303ff0a42711e00a7
aridepai17 <advaithdepai26@gmail.com>
2025-04-27 22:03:39 +0530

    fibonacci sequence till 13 for points

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -18,6 +18,16 @@ const TaskCard = ({task, updateTaskPoints}:{
     updateTaskPoints: (task: Task, points: number) => void
     }) => {
     const points = task.points || 0
+
+    const updatePoints = (direction: 'up' | 'down') => {
+        const fib = [0, 1, 2, 3, 5, 8, 13]
+        const index = fib.indexOf(points)
+        const nextIndex = direction === 'up' ? index + 1 : index - 1
+        const newPoints = fib[nextIndex]
+        if(newPoints){
+            updateTaskPoints(task, newPoints)
+        }
+    }
     return <div className = "text-4xl border rounded-lg px-2 m-2 bg-gray-50 w-56">
         <div className = "text-base font-basepy-2">
             {task.title}
@@ -31,9 +41,9 @@ const TaskCard = ({task, updateTaskPoints}:{
                 {task.priority === 'high' && highPriorityIcon}   
             </div>
             <div className='flex gap-2' items-center>
-                <button onClick = {() => updateTaskPoints(task, points - 1)}>-</button>
+                <button onClick = {() => updatePoints('down')}>-</button>
                 <div className = 'font-bold'>{points}</div>
-                <button onClick = {() => updateTaskPoints(task, points + 1)}>+</button>
+                <button onClick = {() => updatePoints('up')}>+</button>
             </div>
         </div>
     </div>
