1ba6ce278bd51138e1dfeca747ffbda1b548bef3
aridepai17 <advaithdepai26@gmail.com>
2025-04-27 19:09:53 +0530

    First React Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import './App.css'
+import TaskCard from './task-card'
 
 function App() {
   const title = "Do Market Research"
   const id = "Bus - 1"
   const points = 5
-  
 
   return (
-    <div className = "text-4xl border rounded-lg px-2 m-2 bg-gray-50">
-      <div className = "text-base font-bold py-2">
-        {title}
-      </div>
-      <div className='flex-gap-4 justify-between py-2 text-gray-700'>
-        <div>{id}</div>
-        <div>{points}</div>
-      </div>
-    </div>
+    <>
+      <TaskCard title={title} id={id} points={points} />
+      <TaskCard title = "Competitor Analysis" id = "Bus - 2" points = {3} />
+      <TaskCard title = "Customer Interviews" id = "Bus - 3" points = {8} />
+      <TaskCard title = "User Testing" id = "Bus - 4" points = {2} />
+    </>
   )
 }
 
---
+++ src/task-card.tsx
@@ -0,0 +1,11 @@
+const TaskCard = ({title, id, points}) => {
+    return <div className = "text-4xl border rounded-lg px-2 m-2 bg-gray-50">
+        <div className = "text-base font-bold py-2">
+            {title}
+        </div>
+        <div className='flex-gap-4 justify-between py-2 text-gray-700'>
+            <div>{id}</div>
+            <div>{points}</div>
+        </div>
+    </div>
+}
