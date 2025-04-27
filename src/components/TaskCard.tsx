e561d906bdf19a49150309214a1a7b2b97b77975
aridepai17 <advaithdepai26@gmail.com>
2025-04-27 21:57:23 +0530

    task points

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,20 +1,23 @@
 import { Task } from "../utils/data-tasks";
+import { useState } from "react";
 
 const lowPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 9l7 7 7-7" />
+<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 9l7 7 7-7" />
 </svg>
 const mediumPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10h14" />
-  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 14h14" />
+<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10h14" />
+<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 14h14" />
 </svg>
 const highPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
+<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
 </svg>
 
 
-const TaskCard = ({task}:{
+const TaskCard = ({task, updateTaskPoints}:{
     task: Task
+    updateTaskPoints: (task: Task, points: number) => void
     }) => {
+    const points = task.points || 0
     return <div className = "text-4xl border rounded-lg px-2 m-2 bg-gray-50 w-56">
         <div className = "text-base font-basepy-2">
             {task.title}
@@ -27,7 +30,11 @@ const TaskCard = ({task}:{
                 {task.priority === 'medium' && mediumPriorityIcon}
                 {task.priority === 'high' && highPriorityIcon}   
             </div>
-            <div>{task.points}</div>
+            <div className='flex gap-2' items-center>
+                <button onClick = {() => updateTaskPoints(task, points - 1)}>-</button>
+                <div className = 'font-bold'>{points}</div>
+                <button onClick = {() => updateTaskPoints(task, points + 1)}>+</button>
+            </div>
         </div>
     </div>
 }
