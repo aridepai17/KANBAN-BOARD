9db8573f5e447996c5a375703b445e694900e52f
aridepai17 <advaithdepai26@gmail.com>
2025-04-27 19:31:57 +0530

    types

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import './App.css'
-import TaskCard from './task-card'
+import  TaskCard from './task-card'
+import  { Task } from './data-tasks'
+
+export type Task = {
+  title: string,
+  id: string,
+  points?: number
+}
 
 function App() {
-  const title = "Do Market Research"
-  const id = "Bus - 1"
-  const points = 5
+  const task: Task = {
+    title: "Task 1",
+    id: "1",
+    points: 5
+  }
 
   return (
     <>
-      <TaskCard title={title} id={id} points={points} />
-      <TaskCard title = "Competitor Analysis" id = "Bus - 2" points = {3} />
-      <TaskCard title = "Customer Interviews" id = "Bus - 3" points = {8} />
-      <TaskCard title = "User Testing" id = "Bus - 4" points = {2} />
+      <TaskCard task = {task} />
     </>
   )
 }
